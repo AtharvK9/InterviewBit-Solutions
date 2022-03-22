3f53513216e0082f1c25fb08925a40808757d105
Atharva Kasar <85502961+AtharvK9@users.noreply.github.com>
2022-03-22 23:49:31 +0530

    Update 1.Gas Station.cpp

--- Amazon Interview Questions/1.Gas Station.cpp
+++ Amazon Interview Questions/1.Gas Station.cpp
@@ -1,3 +1,14 @@
+/*Solution Approach
+The bruteforce solution should be obvious. Start from every i, and check to see if every point is reachable with the gas available.
+Return the first i for which you can complete the trip without the gas reaching a negative number.
+This approach would however be quadratic.
+
+Lets look at how we can improve.
+1) If sum of gas is more than sum of cost, does it imply that there always is a solution ?
+2) Lets say you start at i, and hit first negative of sum(gas) - sum(cost) at j. We know TotalSum(gas) - TotalSum(cost) > 0.
+What happens if you start at j + 1 instead ? Does it cover the validity clause for i to j already ?*/
+
+
 int Solution::canCompleteCircuit(const vector<int> &gas, const vector<int> &cost) {
     int fuel = 0, start_i = 0, sum = 0;
     
